f9680c8ce680850c92bd12e337892e3b01136a13
Alex Orphan <nuk3fluk3@gmail.com>
2017-03-22 21:03:17 +0300

    port issue fix

--- server.js
+++ server.js
@@ -12,15 +12,10 @@ const sendIndex = (req, res) => {
 app.get('/chat', sendIndex)
 app.get('/login', sendIndex)
 
-<<<<<<< HEAD
-const server = app.listen(3000, function () {
-  console.log("Listening on port 3000!")
-=======
 const port = process.env.PORT || 8080
 
 const server = app.listen(port, function () {
 	console.log("Listening on port 80")
->>>>>>> afc332c14a07079261635bfa827ed459516d751c
 })
 
 socketServer(server)
